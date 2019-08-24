fad1c54b9a4869ad193abefabedc2a1224679f6c
jose escurra <jocscurra_1@hotmail.com>
2019-08-23 20:17:27 -0400

    segundo cambio

--- proyecto1/Program.cs
+++ proyecto1/Program.cs
@@ -11,6 +11,7 @@ namespace proyecto1
         static void Main(string[] args)
         {
             Console.WriteLine("Primer cambio");
+            Console.WriteLine("segundo cambio");
         }
     }
 }
